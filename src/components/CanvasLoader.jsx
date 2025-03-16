3f192514e19988cadef804e91c003e9678fdf9be
Abdullah Zafar <abdullahzafar70@gmail.com>
2025-03-16 15:00:04 +0000

    Rename Loader component to CanvasLoader and enhance loading visuals; add mobile responsiveness to Computers component

---
+++ src/components/CanvasLoader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Html, useProgress} from '@react-three/drei';
+
+const Loader = () => {
+  const {progress} = useProgress();
+
+  return (
+    <Html>
+      <span className='canvas-load'></span>
+      <p className='text-secondary text-[18px] font-medium'>
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  )
+}
+
+export default Loader
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, Stars, useGLTF } from '@react-three/drei';
-import Loader from '../Loader';
+import CanvasLoader from '../CanvasLoader';
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    setIsMobile(mediaQuery.matches);
+
+    const handler = (e) => setIsMobile(e.matches);
+    mediaQuery.addEventListener('change', handler);
+
+    return () => mediaQuery.removeEventListener('change', handler);
+
+  }, [])
 
   return (
     <Canvas
@@ -14,26 +26,25 @@ const Computers = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<Loader/>}>
+      <Suspense fallback={<CanvasLoader/>}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
         <mesh>
-          <ambientLight intensity={0.5} groundColor="white" />
+          <ambientLight intensity={1.5} groundColor="white" />
           <pointLight intensity={1} />
           <spotLight
             position={[-20, 50, 10]}
             angle={0.15}
-            castShadow
             shadow-mapSize={1024}
           />
           <primitive
             object={computer.scene}
-            scale={0.6}
-            position={[0, -2.25, -1.5]}
-            rotation={[-0.01, -0.2, -0.1]}
+            scale={isMobile ? 0.3 : 0.6}
+            position={isMobile ? [0, -1.5, -0.5] : [0, -2.25, -1.5]}
+            rotation={isMobile ? [-0.01, -0.7, -0.1] : [-0.01, -0.2, -0.1]}
           />
         </mesh>
       </Suspense>
